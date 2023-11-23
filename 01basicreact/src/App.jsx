18bbdbf5baabaea4c9f082a484d5d4d34062a565
soyeb101 <soyeb10191@gmail.com>
2023-11-23 15:16:00 +0600

    counter app

--- 01basicreact/src/App.jsx
+++ 01basicreact/src/App.jsx
@@ -1,9 +1,14 @@
 import Soyeb from "./Soyeb";
 
 function App() {
+  const username = "soyeb islam";
+
   return (
     <>
       <Soyeb />
+
+      <h1>React practice</h1>
+      {<p>My name is {username} </p>}
     </>
   );
 }
