8d99e39b6cd651599a19e8761178e20d93c9bb26
Md. Soyeb Islam <soyeb10191@gmail.com>
2023-12-16 21:33:17 +0600

    components parts

---
+++ blog-appwrite/src/components/Select.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable react/prop-types */
+import React, { useId } from 'react';
+
+const Select = ({
+    options,
+    label,
+    className = '',
+  ...props
+}, ref) => {
+    const id = useId();
+  return (
+    <div className='w-full'>
+        {label && <label htmlFor={id}
+        className=''
+        >
+            
+            </label>}
+           <select
+           {...props}
+           id={id}
+           ref={ref}
+           className={`px-3 py-2 rounded-lg bg-white to-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+           >
+            {options?.map((option) => (
+                <option key={option}  value={option}>
+                    {option}
+                </option>
+            ))}
+           </select>
+    </div>
+  )
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export default React.forwardRef(Select)
--- blog-appwrite/src/components/index.js
+++ blog-appwrite/src/components/index.js
@@ -1,4 +1,8 @@
 import Footer from "./Footer/Footer";
 import { Header } from "./Header/Header";
+import LogoutBtn from "./Header/LogoutBtn";
+import { Logo } from "./Logo";
+import Container from "./container/Container";
 
-export { Footer, Header };
+export { Container, Footer, Header, Logo, LogoutBtn };
+
