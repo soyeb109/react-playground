108a1ee4dbd5824879fc90cb6a1c2af2dee28aba
Md. Soyeb Islam <soyeb10191@gmail.com>
2023-12-17 21:53:20 +0600

    RTE

--- blog-appwrite/src/components/index.js
+++ blog-appwrite/src/components/index.js
@@ -2,7 +2,8 @@ import Footer from "./Footer/Footer";
 import { Header } from "./Header/Header";
 import LogoutBtn from "./Header/LogoutBtn";
 import { Logo } from "./Logo";
+import RTE from "./RTE";
 import Container from "./container/Container";
 
-export { Container, Footer, Header, Logo, LogoutBtn };
+export { Container, Footer, Header, Logo, LogoutBtn, RTE };
 
