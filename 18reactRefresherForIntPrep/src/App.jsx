572c3f3c8e7f61dd4ad503710a10553b157d8a8e
Md. Soyeb Islam <soyeb10191@gmail.com>
2023-12-18 18:24:33 +0600

    useMemo with costly operation

--- 18reactRefresherForIntPrep/src/App.jsx
+++ 18reactRefresherForIntPrep/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useMemo, useState } from 'react'
 import Button from './components/Button'
 import ShowCount from './components/ShowCount'
 import Title from './components/Title'
@@ -16,13 +16,20 @@ export default function App() {
 
   const incerementByFive = useCallback(() => {
     setCount2((prevCount) => prevCount + 5)
-  },[])
+  },[]) // react will forget the function if the only dependency changes
   // if we have used a count in this function we had to giv the dependencies in the array count because count is in the outside 
-  
+
+  const isEvenOrOdd = useMemo(() => {
+    let i = 0;
+    while (i < 1000000000) i += 1; //costly operation
+    return count % 2 === 0;
+  },[count])
+
   return (
     <div>
       <Title />
       <ShowCount count={count} title='Counter 1' />
+      <span>{isEvenOrOdd ? 'even' : 'odd'}</span>
       <Button handleClick={incerementByOne}>increment by one</Button>
       <hr />
       <ShowCount count={count2} title='Counter 2' />
